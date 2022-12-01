251562fa67bfe536e6374330c960554e80199c31
Vasili2797 <nashvilliv1@gmail.com>
2022-12-01 11:23:34 -0500

    Added gitignore and sanityTest

--- ControlExtensionProjects/src/test/java/SanityTests.java
+++ ControlExtensionProjects/src/test/java/SanityTests.java
@@ -1,7 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SanityTests {
   @Test
-  public void f() {
+  public void SanityTest() {
+	  
+	  var driverpath="C:\\Users\\nashv\\Downloads\\chromedriver_win32_1\\chromedriver.exe";
+	  System.setProperty("webdriver.chrome.driver", driverpath);
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.google.com/");
+	  var expectedResult = "https://www.google.com/";
+	  
+	  var result=driver.getCurrentUrl();;
+	  
+	  Assert.assertEquals(result, expectedResult);
+	  driver.close();
   }
 }
