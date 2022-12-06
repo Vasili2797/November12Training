a34b3387d6fcdf0e0ee91d656672b522146a083a
Vasili2797 <nashvilliv1@gmail.com>
2022-12-05 19:21:00 -0500

    SelectMenuTests takes the text value of only one item twice

--- ControlExtensionProjects/src/test/java/DriverManagers/ChromeDriverManager.java
+++ ControlExtensionProjects/src/test/java/DriverManagers/ChromeDriverManager.java
@@ -1,5 +1,7 @@
 package DriverManagers;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -12,14 +14,10 @@ public class ChromeDriverManager extends DriverManager {
 
 		var driverPath = "C:\\Users\\nashv\\Downloads\\chromedriver_win32_1\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
-//		WebDriver driver = new ChromeDriver();
-//		driver.get("https://www.google.com/");
-//
-//		var expectedResult = "https://www.google.com/";
-//		var result = driver.getCurrentUrl();
-//
-//		Assert.assertEquals(result, expectedResult);
-//		driver.quit();
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
 
 	}
 }
--- ControlExtensionProjects/src/test/java/Tests/SelectMenuTests.java
+++ ControlExtensionProjects/src/test/java/Tests/SelectMenuTests.java
@@ -7,22 +7,29 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SelectMenuTests {
+	WebDriver driver;
+
 	@Test
 	public void getThirdOptionInStringArray() {
+		// ChromeDriverManager();
 		var driverPath = "C:\\Users\\nashv\\Downloads\\chromedriver_win32_1\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
 
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://demoqa.com/select-menu");
+
 		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/select-menu");
 
 		WebElement t = driver.findElement(By.id("oldSelectMenu"));
 		Select select = new Select(t);
@@ -38,7 +45,7 @@ public class SelectMenuTests {
 	}
 
 	@Test
-	public void getAllOptionsInStringArray() {//They are they same, but assertEquals says not true
+	public void getAllOptionsInStringArray() {// They are the same, but assertEquals says not true
 
 		var driverPath = "C:\\Users\\nashv\\Downloads\\chromedriver_win32_1\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", driverPath);
@@ -65,16 +72,38 @@ public class SelectMenuTests {
 		String[] ValuesOnline = new String[] { "Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet",
 				"Indigo", "Magenta", "Aqua" };
 
-//		StringBuilder stringBuilder = new StringBuilder();
-//		for (int i = 0; i < ValuesOnline.length; i++) {
-//			stringBuilder.append(ValuesOnline[i] + ", ");
-//		}
 		var expected = Arrays.toString(ValuesOnline);
-//		System.out.println(Arrays.toString(ValuesOnline));
-//		System.out.println("The StringBuilder "+stringBuilder);
 
 		Assert.assertEquals(stringArray/* actual */, expected/* expected */);
 
 		driver.quit();
+	}
+
+	@Test
+	public void getTwoSelectedOptions() {
+		var driverPath = "C:\\Users\\nashv\\Downloads\\chromedriver_win32_1\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", driverPath);
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/select-menu");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		Select select = new Select(driver.findElement(By.id("cars")));
+		select.selectByIndex(1);
+		select.selectByIndex(2);
+
+		List<WebElement> list = select.getAllSelectedOptions();
+		list.stream().toArray();
+
+		String[] strArray = new String[list.size()];
+
+		for (WebElement i : list) {
+			for (int j = 0; j <= 2; j++) {
+				strArray[j] = i.getText();
+				System.out.println(strArray[j]);
+			}
+
+		}
 	}
 }
