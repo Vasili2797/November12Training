ef988d917519840a0323d0ffcd5733514f48ec6a
Vasili2797 <nashvilliv1@gmail.com>
2022-12-06 19:10:26 -0500

    Changes in SanityTests and TestBase

--- ControlExtensionProjects/src/test/java/Tests/SanityTests.java
+++ ControlExtensionProjects/src/test/java/Tests/SanityTests.java
@@ -1,14 +1,9 @@
 package Tests;
 
-import java.sql.DriverManager;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import DriverManagers.ChromeDriverManager;
+import DriverManagers.DriverManager;
 import DriverManagers.EdgeDriverManager;;
 
 public class SanityTests {
@@ -16,14 +11,14 @@ public class SanityTests {
 	@Test
 	public void testUsingChromeDriverManager() {
 
-		ChromeDriverManager driver = new ChromeDriverManager();
+		DriverManager driver = new ChromeDriverManager();
 		driver.createDriver();
 	}
 
 	@Test
 	public void testUsingEdgeDriverManager() {
 
-		EdgeDriverManager driver = new EdgeDriverManager();
+		DriverManager driver = new EdgeDriverManager();
 		driver.createDriver();
 	}
 
--- ControlExtensionProjects/src/test/java/Tests/TestBase.java
+++ ControlExtensionProjects/src/test/java/Tests/TestBase.java
@@ -1,25 +1,24 @@
 package Tests;
 
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import DriverManagers.DriverManagerFactory;
 
+public abstract class TestBase {
+	@Test
+	public void setUp() {
+		var browserType = "chrome";
+//		var manager = DriverManagerFactory.getManager(browserType);
+	}
 
-public abstract class TestBase{
-  @Test
-  public void setUp() {
-	  var browserType = "chrome";
-	  var manager = DriverManagerFactory.getManager(browserType);
-  }
-  @BeforeMethod
-  public void beforeMethod() {
-  }
+	@BeforeMethod
+	public void beforeMethod() {
+	}
 
-  @AfterMethod
-  public void afterMethod() {
-  }
+	@AfterMethod
+	public void afterMethod() {
+	}
 
 }
