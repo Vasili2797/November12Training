4d843863e8482f97a8735383f616f502fa5b7a19
Vasili2797 <nashvilliv1@gmail.com>
2023-01-09 02:32:22 -0500

    Made Data.java class and changed pom.xml file

--- my.api.project/src/main/java/Data.java
+++ my.api.project/src/main/java/Data.java
@@ -1,11 +1,49 @@
 
 public class Data {
-	
-	private int id;
+
+	int id;
+	String email;
+	String first_name;
+	String last_name;
+	String data;
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public int getId() {
+		return id;
+	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	public int getId() {
-		return this.id;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
 	}
 }
